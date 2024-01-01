f48a15a5c997251f3350ba78f8b8de7580e3b83b
oldWaffle <155334417+oldWaffle@users.noreply.github.com>
2024-01-01 14:48:45 -0600

    Update saveInfo.js

--- saveInfo.js
+++ saveInfo.js
@@ -1,12 +1,1 @@
-let autoSaveTimer = 60; //In Seconds
 
-function saveGame() {
-    localStorage.setItem("woodNumber", woodnum);
-    localStorage.setItem("merchantHasArrived", merchantArrival);
-    localStorage.setItem("hasFishingPole", fishingPoleTrue);
-    gameConsoleElement.innerHTML += "Game has been saved. <br />"
-}
-
-
-
-var intervalKeyEvents = setInterval(saveGame, autoSaveTimer * 1000)
