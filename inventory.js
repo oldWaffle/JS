31bbe54d8b0b9b17bc1a183c13d2c968fca5c8f0
oldWaffle <155334417+oldWaffle@users.noreply.github.com>
2024-01-03 12:24:37 -0600

    Update inventory.js

--- inventory.js
+++ inventory.js
@@ -1,8 +1,1 @@
-let inventory = ""
 
-function checkInventory() {
-    inventory = ""
-    inventory += "Inventory: <br/>" + "Wood: " + woodnum + "<br/> Fish: " + fishnum + "<br/>";
-    gameConsoleElement.innerHTML += inventory;
-
-}
